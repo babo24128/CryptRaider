20cc683676e015e53caa669dbcd49918733aa769
babo24128 <babo24128@naver.com>
2024-07-21 17:50:09 +0900

    FineComponentByClass() & nullptr

--- Source/CryptRaider/Grabber.cpp
+++ Source/CryptRaider/Grabber.cpp
@@ -4,6 +4,7 @@
 #include "Grabber.h"
 #include "Engine/World.h"
 #include "DrawDebugHelpers.h"
+#include "PhysicsEngine/PhysicsHandleComponent.h"
 
 // Sets default values for this component's properties
 UGrabber::UGrabber()
@@ -21,8 +22,15 @@ void UGrabber::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
-	
+	UPhysicsHandleComponent* PhysicsHandle = GetOwner()->FindComponentByClass<UPhysicsHandleComponent>();
+	if (PhysicsHandle != nullptr)
+	{
+		UE_LOG(LogTemp, Display, TEXT("Got Physics Handle : %s"), *PhysicsHandle->GetName());
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("No Physics Handle Found!"));
+	}
 }
 
 
