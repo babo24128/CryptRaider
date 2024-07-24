fc58b16a45499556b8410baeb63f9e475aafe4ee
babo24128 <babo24128@naver.com>
2024-07-24 18:17:46 +0900

    생성자

--- Source/CryptRaider/TriggerComponent.cpp
+++ Source/CryptRaider/TriggerComponent.cpp
@@ -3,9 +3,21 @@
 
 #include "TriggerComponent.h"
 
+UTriggerComponent::UTriggerComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+
+	UE_LOG(LogTemp, Display, TEXT("Constructing"))
+}
+
 void UTriggerComponent::BeginPlay()
 {
 	Super::BeginPlay();
 
 	UE_LOG(LogTemp, Display, TEXT("Trigger Component Alive"));
+}
+
+void UTriggerComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 }
